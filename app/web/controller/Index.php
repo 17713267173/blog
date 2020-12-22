56e0a48d46f7d92d6b11c039b8c87d372a304486
chenjinbao <845325935@qq.com>
2020-12-22 13:42:35 +0800

    解决跨域请求

--- app/web/controller/Index.php
+++ app/web/controller/Index.php
@@ -5,6 +5,11 @@ use app\BaseController;
 use think\facade\View;
 use app\web\model\Admins;
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE');
+header('Access-Control-Max-Age: 1728000');
+
 class Index extends BaseController
 {
     public function index()
